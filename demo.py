dd9e39417715d4549ab8ac06dc4a5ef8b4a230fb
Jagmohan Solanki <41115485+jagmohan-solanki@users.noreply.github.com>
2020-01-27 21:33:35 +0530

    2time modified in demo.py

--- demo.py
+++ demo.py
@@ -2,3 +2,7 @@
 print("Hello world")
 for i in range(10):
     print("the value of i is : ",i)
+'''this is modification for 2 time'''
+for j in range(11,20):
+    print("the value of j is : ",j)
+    
