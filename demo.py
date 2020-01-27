a9f8f8051048025f251a23f5d0b8352937822e25
Jagmohan Solanki <41115485+jagmohan-solanki@users.noreply.github.com>
2020-01-27 21:36:14 +0530

    3rd time modification in demo.py python file

--- demo.py
+++ demo.py
@@ -6,3 +6,7 @@ for i in range(10):
 for j in range(11,20):
     print("the value of j is : ",j)
     
+ '''this is modification for 3 time'''
+for k in range(11,30):
+    print("the value of j is : ",k)
+    
