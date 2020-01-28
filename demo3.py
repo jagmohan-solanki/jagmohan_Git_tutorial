b546efc732611bcfe6db5faa4c6689accf8456ca
lajulsoni <lajul.soni24@gmail.com>
2020-01-28 15:51:33 +0530

    commit succesfully

---
+++ demo3.py
@@ -0,0 +1,26 @@
+'''this is demo.py python file'''
+print("Hello world")
+for i in range(10):
+    print("the value of i is : ",i)
+'''this is modification for 2 time'''
+for j in range(11,20):
+    print("the value of j is : ",j)
+    
+ '''this is modification for 3 time'''
+for k in range(11,30):
+    print("the value of j is : ",k)
+    
+## DESCRIPTION :::===>
+'''
+here we are adding some new feature and will create into new branch 
+
+'''
+###  DHABA
+# VeG
+1) Roti
+2) vegitables
+3)soup
+print("this is final in jms_DHABA branch")
+=======
+print("this modification is in master branch")
+    
